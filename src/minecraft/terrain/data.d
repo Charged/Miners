0739f88ae1582574883a364c9e43f02925c90cf9
Reiner Gerecke <mr.squidd@gmail.com>
2011-05-12 05:19:07 +0800

    mc: Added signs attached to walls.
    Signed-off-by: Reiner Gerecke <mr.squidd@gmail.com>

--- src/minecraft/terrain/data.d
+++ src/minecraft/terrain/data.d
@@ -104,7 +104,7 @@ BlockDescriptor tile[256] = [
 	{ false, Stuff,     {  3,  5 }, {  3,  5 }, "ladder" },
 	{ false, Stuff,     {  0,  0 }, {  0,  0 }, "rails" },
 	{ false, Stuff,     {  0,  1 }, {  0,  1 }, "clobblestone stairs" },
-	{ false, Stuff,     {  0,  0 }, {  0,  0 }, "wall sign" },
+	{ false, Stuff,     {  4,  0 }, {  4,  0 }, "wall sign" },
 	{ false, Stuff,     {  0,  0 }, {  0,  0 }, "lever" },
 	{ false, Stuff,     {  0,  0 }, {  0,  0 }, "stone pressure plate" },
 	{ false, Stuff,     {  2,  5 }, {  2,  6 }, "iron door" },
