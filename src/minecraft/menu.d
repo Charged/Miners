5735557340296e4c6dd774eb415b68b08965fd7f
Jakob Bornecrantz <wallbraker@gmail.com>
2011-05-29 13:49:41 +0200

    mc: Reorg menu code
    Signed-off-by: Jakob Bornecrantz <wallbraker@gmail.com>

--- src/minecraft/menu.d
+++ src/minecraft/menu.d
@@ -22,7 +22,7 @@ protected:
 	Router router;
 	Runner levelRunner;
 
-	GfxTextureTarget infoTexture;
+	GfxTextureTarget menuTexture;
 
 	LevelButton currentButton;
 	HeaderContainer hc;
@@ -40,7 +40,7 @@ public:
 	{
 		this.router = r;
 
-		makeInfoTexture();
+		makeLevelSelector();
 
 		keyboard = CtlInput().keyboard;
 		mouse = CtlInput().mouse;
@@ -58,8 +58,8 @@ public:
 		delete hc;
 		delete levelRunner;
 
-		if (infoTexture !is null)
-			infoTexture.dereference();
+		if (menuTexture !is null)
+			menuTexture.dereference();
 	}
 
 	void resize(uint w, uint h)
@@ -75,16 +75,16 @@ public:
 		if (levelRunner !is null)
 			levelRunner.render(rt);
 
-		if (infoTexture is null)
+		if (menuTexture is null)
 			return;
 
-		hc.x = (rt.width - infoTexture.width) / 2;
-		hc.y = (rt.height - infoTexture.height) / 2;
+		hc.x = (rt.width - menuTexture.width) / 2;
+		hc.y = (rt.height - menuTexture.height) / 2;
 
 		auto d = new GfxDraw();
 		d.target = rt;
 		d.start();
-		d.blit(infoTexture, hc.x, hc.y);
+		d.blit(menuTexture, hc.x, hc.y);
 		d.stop();
 	}
 
@@ -110,99 +110,6 @@ public:
 		inControl = false;
 	}
 
-protected:
-	class CloseButton : public Button
-	{
-	protected:
-		MinecraftLevelInfo info;
-
-	public:
-		this(Container p, int x, int y)
-		{
-			this.info = info;
-			super(p, x, y, "Close", 8);
-		}
-
-		void mouseDown(CtlMouse, int x, int y, uint button)
-		{
-			if (levelRunner !is null)
-				router.switchTo(levelRunner);
-		}
-	}
-
-	class QuitButton : public Button
-	{
-	protected:
-		MinecraftLevelInfo info;
-
-	public:
-		this(Container p, int x, int y)
-		{
-			this.info = info;
-			super(p, x, y, "Quit", 8);
-		}
-
-		void mouseDown(CtlMouse, int x, int y, uint button)
-		{
-			if (levelRunner !is null) {
-				router.deleteMe(levelRunner);
-				levelRunner = null;
-			}
-			router.deleteMe(this.outer);
-		}
-	}
-
-	class LevelButton : public Button
-	{
-	protected:
-		MinecraftLevelInfo info;
-
-	public:
-		this(Container p, MinecraftLevelInfo info)
-		{
-			this.info = info;
-			super(p, 0, 0, info.name, 32);
-		}
-
-		void mouseDown(CtlMouse, int x, int y, uint button)
-		{
-			if (currentButton is this)
-				return;
-
-			delete levelRunner;
-			levelRunner = router.loadLevel(info.dir);
-			currentButton = this;
-		}
-	}
-
-	class LevelSelector : public Container
-	{
-	public:
-		this(Container p, int x, int y)
-		{
-			auto levels = scanForLevels();
-			char[] str;
-
-			foreach(l; levels) {
-				if (!l.beta)
-					continue;
-
-				new LevelButton(this, l);
-			}
-
-			int pos;
-			foreach(c; getChildren()) {
-				c.x = 0;
-				c.y = pos;
-				pos += c.h;
-				w = cast(int)fmax(w, c.x + c.w);
-				h = cast(int)fmax(h, c.y + c.h);
-			}
-
-			super(p, x, y, w, h);
-		}
-	}
-
 private:
 	void keyDown(CtlKeyboard kb, int sym)
 	{
@@ -218,16 +125,21 @@ private:
 			router.switchTo(this);
 	}
 
-	void makeInfoTexture()
+	void makeLevelSelector()
 	{
-		char[] str = selectText;
+		if (menuTexture !is null)
+			menuTexture.dereference();
+
+		assert(hc is null);
 		hc = new HeaderContainer(Color4f(0, 0, 0, 0.8),
 					 "Charged Miners",
 					 Color4f(0, 0, 1, 0.8));
 		auto it = new Text(hc, 0, 0, selectText);
-		auto ls = new LevelSelector(hc, 0, it.h);
-		auto cb = new CloseButton(hc, 0, it.h + ls.h + 16);
-		auto qb = new QuitButton(hc, cb.w + 16, it.h + ls.h + 16);
+		auto ls = new LevelSelector(hc, 0, it.h, &selectMenuSelect);
+		auto cb = new Button(hc, 0, it.h + ls.h + 16, "Close", 8);
+		auto qb = new Button(hc, cb.w + 16, it.h + ls.h + 16, "Quit", 8);
+		cb.pressed ~= &selectMenuClose;
+		qb.pressed ~= &selectMenuQuit;
 		hc.repack();
 
 		auto center = hc.plane.w / 2;
@@ -244,9 +156,39 @@ private:
 		// Paint the texture.
 		hc.paint();
 
-		if (infoTexture !is null)
-			infoTexture.dereference();
-		infoTexture = hc.getTarget();
+		menuTexture = hc.getTarget();
+	}
+
+	/*
+	 *
+	 * Select menu callbacks and text.
+	 *
+	 */
+
+	void selectMenuQuit(Button b)
+	{
+		if (levelRunner !is null) {
+			router.deleteMe(levelRunner);
+			levelRunner = null;
+		}
+		router.deleteMe(this);
+	}
+
+	void selectMenuClose(Button b)
+	{
+		if (levelRunner !is null)
+			router.switchTo(levelRunner);
+	}
+
+	void selectMenuSelect(Button b)
+	{
+		auto lb = cast(LevelButton)b;
+		if (currentButton is lb)
+			return;
+
+		delete levelRunner;
+		levelRunner = router.loadLevel(lb.info.dir);
+		currentButton = lb;
 	}
 
 	const char[] headerTextChars = `Charged Miners`;
@@ -255,4 +197,56 @@ private:
 `Welcome to charged miners, please select level below.
 
 `;
+}
+
+/**
+ * A Button that displays the name of a level.
+ *
+ * Used with the select menu.
+ */
+class LevelButton : public Button
+{
+public:
+	MinecraftLevelInfo info;
+
+public:
+	this(Container p, MinecraftLevelInfo info)
+	{
+		this.info = info;
+		super(p, 0, 0, info.name, 32);
+	}
+}
+
+/**
+ * A Container for LevelButtons.
+ *
+ * Used with the select menu.
+ */
+class LevelSelector : public Container
+{
+public:
+	this(Container p, int x, int y, void delegate(Button b) dg)
+	{
+		auto levels = scanForLevels();
+		char[] str;
+
+		foreach(l; levels) {
+			if (!l.beta)
+				continue;
+
+			auto lb = new LevelButton(this, l);
+			lb.pressed ~= dg;
+		}
+
+		int pos;
+		foreach(c; getChildren()) {
+			c.x = 0;
+			c.y = pos;
+			pos += c.h;
+			w = cast(int)fmax(w, c.x + c.w);
+			h = cast(int)fmax(h, c.y + c.h);
+		}
+
+		super(p, x, y, w, h);
+	}
 }
