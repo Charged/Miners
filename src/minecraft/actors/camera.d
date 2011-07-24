8862d1686fd55d07eaad2e4726dda1494bb5c9a9
Jakob Bornecrantz <wallbraker@gmail.com>
2011-07-24 18:11:37 +0200

    mc: Center the iso camera height at a fixed altitude
    Signed-off-by: Jakob Bornecrantz <wallbraker@gmail.com>

--- src/minecraft/actors/camera.d
+++ src/minecraft/actors/camera.d
@@ -32,6 +32,11 @@ protected:
 	uint ppb;
 
 	/**
+	 * Center the iso camera position height at this level.
+	 */
+	double isoCenterHeight;
+
+	/**
 	 * The width of a square across the screen, in game units.
 	 *
 	 * Using the Pythagorean theorem yeilds:
@@ -51,12 +56,20 @@ protected:
 	 */
 	const isoHeightLength = 0.6324555320336758663997787088865437067439110278650433653715;
 
+	/**
+	 * When moving the iso camera to the height of 64 we need to multiply the
+	 * difference between the current height and 64 with a constant. To get
+	 * the length that the point must travel along the view of the iso camera.
+	 */
+	const isoHeightDiff = 0.4472135954999579392818347337462552470881236719223051448541;
+
 public:
 	this(World w)
 	{
 		super(w);
 
 		this.ppb = 32;
+		this.isoCenterHeight = 64.0;
 
 		pcam = new GfxProjCamera();
 		icam = new GfxIsoCamera(800, 600, -200, 200, GfxIsoCamera.TwoToOne);
@@ -87,6 +100,17 @@ public:
 		pcam.ratio = ratio;
 	}
 
+	void setPosition(ref Point3d pos)
+	{
+		pcam.setPosition(pos);
+
+		// Center the iso camera at a certain height
+		auto diff = pos.y - isoCenterHeight;
+		auto vec = Quatd(PI/4, atan(-.5), 0).rotateHeading;
+		vec.scale(diff/isoHeightDiff);
+		icam.setPosition(pos + vec);
+	}
+
 	final void pixelsPerBlock(uint ppb)
 	{
 		assert(ppb != 0);
@@ -101,6 +125,11 @@ public:
 		(cast(World)w).t.setViewRadii(cast(int)(value / 16 + 1));
 	}
 
+	final void isoHeight(double value)
+	{
+		isoCenterHeight = value / 2;
+	}
+
 	final GfxCamera current() { return c; }
 	final bool iso() { return c is icam; }
 	final bool proj() { return c is pcam; }
@@ -111,7 +140,6 @@ public:
 	final double far() { return pcam.far; }
 	final double near() { return pcam.near; }
 
-	void setPosition(ref Point3d pos) { pcam.setPosition(pos); icam.setPosition(pos); }
 	void getPosition(out Point3d pos) { pcam.getPosition(pos); }
 	void setRotation(ref Quatd rot) { pcam.setRotation(rot); }
 	void getRotation(out Quatd rot) { pcam.getRotation(rot); }
