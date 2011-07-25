a6ac97e44a2db3e1bf8903ae4954adbcc32ee1cc
Jakob Bornecrantz <wallbraker@gmail.com>
2011-07-25 18:46:23 +0200

    math: Extend Matrix3x3 with more functions
    Signed-off-by: Jakob Bornecrantz <wallbraker@gmail.com>

--- src/charge/math/matrix3x3d.d
+++ src/charge/math/matrix3x3d.d
@@ -2,10 +2,14 @@
 // See copyright notice in src/charge/charge.d (GPLv2 only).
 module charge.math.matrix3x3d;
 
+import charge.math.vector3d;
+import charge.math.point3d;
 import charge.math.quatd;
+
 
 struct Matrix3x3d
 {
+public:
 	union {
 		double[3][3] m;
 		struct {
@@ -15,6 +19,7 @@ struct Matrix3x3d
 		}
 	}
 
+public:
 	void opAssign(Quatd q) {
 		a = 1 - 2 * q.y * q.y - 2 * q.z * q.z;
 		b =     2 * q.x * q.y - 2 * q.w * q.z;
@@ -27,5 +32,54 @@ struct Matrix3x3d
 		g =     2 * q.x * q.z - 2 * q.w * q.y;
 		h =     2 * q.y * q.z + 2 * q.w * q.x;
 		i = 1 - 2 * q.x * q.x - 2 * q.y * q.y;
+	}
+
+	Vector3d opMul(Vector3d v)
+	{
+		Vector3d result;
+		result.x = v.x * m[0][0] + v.y * m[0][1] + v.z * m[0][2];
+		result.y = v.x * m[1][0] + v.y * m[1][1] + v.z * m[1][2];
+		result.z = v.x * m[2][0] + v.y * m[2][1] + v.z * m[2][2];
+		return result;
+	}
+
+	Point3d opMul(Point3d p)
+	{
+		Point3d result;
+		result.x = p.x * m[0][0] + p.y * m[0][1] + p.z * m[0][2];
+		result.y = p.x * m[1][0] + p.y * m[1][1] + p.z * m[1][2];
+		result.z = p.x * m[2][0] + p.y * m[2][1] + p.z * m[2][2];
+		return result;
+	}
+
+	void inverse()
+	{
+		Matrix3x3d temp;
+
+		temp.m[0][0] = m[1][1] * m[2][2] - m[1][2] * m[2][1];
+		temp.m[1][0] = m[1][2] * m[2][0] - m[1][0] * m[2][2];
+		temp.m[2][0] = m[1][0] * m[2][1] - m[1][1] * m[2][0];
+		temp.m[0][1] = m[0][2] * m[2][1] - m[0][1] * m[2][2];
+		temp.m[1][1] = m[0][0] * m[2][2] - m[0][2] * m[2][0];
+		temp.m[2][1] = m[0][1] * m[2][0] - m[0][0] * m[2][1];
+		temp.m[0][2] = m[0][1] * m[1][2] - m[0][2] * m[1][1];
+		temp.m[1][2] = m[0][2] * m[1][0] - m[0][0] * m[1][2];
+		temp.m[2][2] = m[0][0] * m[1][1] - m[0][1] * m[1][0];
+
+		auto det = m[0][0] * temp.m[0][0] +
+			   m[0][1] * temp.m[1][0] +
+			   m[0][2] * temp.m[2][0];
+
+		auto invDet = 1 / det;
+
+		m[0][0] = temp.m[0][0] * invDet;
+		m[1][0] = temp.m[1][0] * invDet;
+		m[2][0] = temp.m[2][0] * invDet;
+		m[0][1] = temp.m[0][1] * invDet;
+		m[1][1] = temp.m[1][1] * invDet;
+		m[2][1] = temp.m[2][1] * invDet;
+		m[0][2] = temp.m[0][2] * invDet;
+		m[1][2] = temp.m[1][2] * invDet;
+		m[2][2] = temp.m[2][2] * invDet;
 	}
 }
