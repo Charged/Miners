fd3f8c696efb3bbe6ba10bbe8ed71e922fd3eda5
Jakob Bornecrantz <wallbraker@gmail.com>
2011-10-01 21:06:21 +0200

    util: Add HTML to XML escape converter
    Signed-off-by: Jakob Bornecrantz <wallbraker@gmail.com>

---
+++ src/charge/util/html.d
@@ -0,0 +1,29 @@
+// Copyright © 2011, Jakob Bornecrantz.  All rights reserved.
+// See copyright notice in src/charge/charge.d (GPLv2 only).
+module charge.util.html;
+
+/**
+ * Turns a html escapce escape sequence into XML one.
+ * In short only keeps '"', ''', '&', '>' and '<' escaped.
+ *
+ * Only accepts the full sequence of characters for example "&amp;".
+ *
+ * XXX: Not the full set of characters see:
+ * http://en.wikipedia.org/wiki/List_of_XML_and_HTML_character_entity_references
+ */
+char[] htmlToXmlEscape(char[] text)
+{
+	switch(text) {
+	/* XML */
+	case "&qout;": return "&qout;";
+	case "&apos;": return "&apos;";
+	case "&amp;": return "&amp;";
+	case "&lt;": return "&lt;";
+	case "&gt;": return "&gt;";
+	/* HTML */
+	case "&copy;": return "©";
+	case "&dagger;": return "†";
+	default:
+		return null;
+	}
+}
