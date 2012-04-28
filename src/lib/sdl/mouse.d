5e990170460a2bf40a7d0d535fc8a1a110adb2fc
Jakob Bornecrantz <wallbraker@gmail.com>
2012-04-28 21:49:14 +0200

    lib: Add SDL_WarpMouse function
    Signed-off-by: Jakob Bornecrantz <wallbraker@gmail.com>

--- src/lib/sdl/mouse.d
+++ src/lib/sdl/mouse.d
@@ -2,6 +2,8 @@
 // See copyright in src/lib/sdl/sdl.d (LGPLv2+).
 module lib.sdl.mouse;
 
+import lib.sdl.types;
+
 version (DynamicSDL)
 {
 import lib.loader;
@@ -9,14 +11,17 @@ import lib.loader;
 package void loadSDL_Mouse(Loader l)
 {
 	loadFunc!(SDL_ShowCursor)(l);
+	loadFunc!(SDL_WarpMouse)(l);
 }
 extern (C):
 
 int (*SDL_ShowCursor)(int toggle);
+void (*SDL_WarpMouse)(Uint16 x, Uint16 y);
 }
 else
 {
 extern (C):
 
 int SDL_ShowCursor(int toggle);
+void SDL_WarpMouse(Uint16 x, Uint16 y);
 }
