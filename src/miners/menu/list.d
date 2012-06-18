05af9df7ac0178ce7fc4dfae467b24931440a583
Jakob Bornecrantz <wallbraker@gmail.com>
2012-06-18 03:47:44 +0200

    mc/classic: Code style and less magic numbers
    Signed-off-by: Jakob Bornecrantz <wallbraker@gmail.com>

--- src/miners/menu/list.d
+++ src/miners/menu/list.d
@@ -13,11 +13,6 @@ import miners.interfaces;
 import miners.menu.base;
 
 
-const char[] header = `Classic`;
-const uint childHeight = 424;
-const uint childWidth = 9*16;
-
-
 class ClassicServerListMenu : public MenuBase
 {
 private:
@@ -41,7 +36,19 @@ class ListView : public Component
 private:
 	Router r;
 
+	const darkGrey = Color4f(0.05, 0.05, 0.05, 0.8);
+	const lightGrey = Color4f(0.7, 0.7, 0.7, 1);
+	const red = Color4f(1, .3, .3, 1);
+	const green = Color4f(.3, 1, .3, 1);
+	alias Color4f.White white;
+	alias Color4f.Black black;
+
+	const uint rows = 32;
+	const uint rowSize = 11;
 	const uint colums = 160;
+	const uint charSize = 8;
+	const uint charOffsetFromTop = 2;
+
 	int start;
 	ClassicServerInfo[] csis;
 
@@ -52,7 +59,7 @@ private:
 public:
 	this(ClassicServerListMenu cm, int x, int y, ClassicServerInfo[] csis)
 	{
-		super(cm, x, y, 424, 9*16);
+		super(cm, x, y, 424, rowSize*rows);
 
 		this.r = cm.r;
 		this.csis = csis;
@@ -65,23 +72,73 @@ public:
 
 	void repack() {}
 
+
+	/*
+	 *
+	 * Input
+	 *
+	 */
+
+
 protected:
+	void mouseDown(Mouse m, int x, int y, uint b)
+	{
+		if (b != 1)
+			return;
+
+		if (x < w - buttonSize) {
+			int which = (y / rowSize) + start;
+
+			if (which >= csis.length)
+				return;
+
+			r.menu.getClassicServerInfoAndConnect(csis[which]);
+		} else {
+			if (y < h / 2) {
+				if (y < buttonSize)
+					start--;
+				else
+					start -= 13;
+			} else {
+				if (y > (h - buttonSize))
+					start++;
+				else
+					start += 13;
+			}
+
+			if (start + rows >= csis.length)
+				start = cast(int)csis.length - rows;
+
+			if (start < 0)
+				start = 0;
+
+			repaint();
+		}
+	}
+
+
+	/*
+	 *
+	 * Graphics
+	 *
+	 */
+
+
 	void paint(GfxDraw d)
 	{
-
 		if (glyphs is null)
 			makeResources();
 
 		glDisable(GL_BLEND);
-		glColor4fv(Color4f(0.05, 0.05, 0.05, 0.8).ptr);
+		glColor4fv(darkGrey.ptr);
 
 		glBegin(GL_QUADS);
-		for (int i; i < 16; i++) {
+		for (int i; i < rows; i++) {
 			if (((i + start) % 4) >= 2)
 				continue;
 
-			int y1 = i*9;
-			int y2 = y1 + 9;
+			int y1 = i*rowSize;
+			int y2 = y1 + rowSize;
 			int w = this.w - buttonSize;
 
 			glVertex2i( 0, y1 );
@@ -94,11 +151,11 @@ protected:
 		{
 			int runnerLength = h - buttonSize * 2;
 			int l, offset;
-			if (csis.length < 17) {
+			if (csis.length <= rows) {
 				l = runnerLength;
 			} else {
-				l = cast(int)(runnerLength * 16f / cast(float)csis.length);
-				offset = cast(int)((runnerLength - l) * (start / cast(float)(csis.length - 16)));
+				l = cast(int)(runnerLength * rows / cast(float)csis.length);
+				offset = cast(int)((runnerLength - l) * (start / cast(float)(csis.length - rows)));
 			}
 
 			int x1 = w - buttonSize;
@@ -107,7 +164,7 @@ protected:
 			int y1 = buttonSize + offset;
 			int y2 = y1 + l;
 
-			glColor4fv(Color4f.White.ptr);
+			glColor4fv(white.ptr);
 			glVertex2i( x1,  0 );
 			glVertex2f( x1, buttonSize - 1);
 			glVertex2f( x2, buttonSize - 1);
@@ -129,73 +186,43 @@ protected:
 		glEnable(GL_TEXTURE_2D);
 		glBindTexture(GL_TEXTURE_2D, glyphs.id);
 
-		glColor4fv(Color4f.White.ptr);
 		glBegin(GL_QUADS);
 
 		foreach(uint i, csi; csis[start .. $]) {
-			if (i >= 16)
+			if (i >= rows)
 				break;
 
-			int y = i*9+1;
+			int y = i * rowSize + charOffsetFromTop;
 
 			if (csi.webName == "fCraft.net Freebuild [ENG]")
 				glColor4fv(Color4f(1, .3, .3, 1).ptr);
 			else
-				glColor4fv(Color4f.White.ptr);
+				glColor4fv(lightGrey.ptr);
 
 			foreach(uint k, c; csi.webName) {
 				if (k >= colums)
 					break;
 
-				int x =  k * 8 + 8;
+				int x =  k * charSize + charSize;
+
+				if (c == '[')
+					glColor4fv(green.ptr);
 
 				draw(x, y, c);
+
+				if (c == ']')
+					glColor4fv(lightGrey.ptr);
 			}
 		}
 
-		glColor4fv(Color4f.Black.ptr);
-		draw(w - buttonSize + (buttonSize - 8) / 2,    3, 30);
-		draw(w - buttonSize + (buttonSize - 8) / 2, h-10, 31);
+		glColor4fv(black.ptr);
+		draw(w - buttonSize + (buttonSize - charSize) / 2,      3, 30);
+		draw(w - buttonSize + (buttonSize - charSize) / 2, h - 10, 31);
 
 		glEnd();
 
 		glBindTexture(GL_TEXTURE_2D, 0);
 		glDisable(GL_TEXTURE_2D);
-	}
-
-	void mouseDown(Mouse m, int x, int y, uint b)
-	{
-		if (b != 1)
-			return;
-
-		if (x < w - buttonSize) {
-			int which = (y / 9) + start;
-
-			if (which >= csis.length)
-				return;
-
-			r.menu.getClassicServerInfoAndConnect(csis[which]);
-		} else {
-			if (y < h / 2) {
-				if (y < buttonSize)
-					start--;
-				else
-					start -= 13;
-			} else {
-				if (y > (h - buttonSize))
-					start++;
-				else
-					start += 13;
-			}
-
-			if (start + 16 >= csis.length)
-				start = cast(int)csis.length - 16;
-
-			if (start < 0)
-				start = 0;
-
-			repaint();
-		}
 	}
 
 	void makeResources()
@@ -216,12 +243,14 @@ protected:
 		sysReference(&glyphs, null);
 	}
 
-	static void draw(int x1, int y1, char c) {
+	static void draw(int x1, int y1, char c)
+	{
 		float t1 = (c  ) / 256f;
 		float t2 = (c+1) / 256f;
 
-		int x2 = x1 + 8;
-		int y2 = y1 + 8;
+		int x2 = x1 + charSize;
+		int y2 = y1 + charSize;
+
 
 		glTexCoord2f( t1,  0 );
 		glVertex2i  ( x1, y1 );
