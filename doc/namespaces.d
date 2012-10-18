a208b7cbcae2b39b123b05f2f8fbc151a09e6fd5
Jakob Bornecrantz <wallbraker@gmail.com>
2012-10-18 02:26:01 +0200

    charge: Add missing file
    Signed-off-by: Jakob Bornecrantz <wallbraker@gmail.com>

---
+++ doc/namespaces.d
@@ -0,0 +1,74 @@
+// Copyright © 2012, Jakob Bornecrantz.  All rights reserved.
+// See copyright at the bottom of this file (GPLv2 only).
+
+/**
+ * @package charge
+ *
+ * Folder root for all the charge game engine parts.
+ */
+
+/**
+ * @package charge.math
+ *
+ * Folder with math and simple objects.
+ */
+
+/**
+ * @package charge.gfx
+ *
+ * Folder for the graphics subsystem.
+ */
+
+/**
+ * @package charge.sfx
+ *
+ * Folder for the sound subsystem.
+ */
+
+/**
+ * @package charge.ctl
+ *
+ * Folder for the input subsystem, such as Keyboard, Mouse and Joystick.
+ */
+
+/**
+ * @package charge.net
+ *
+ * Folder for the network subsystem.
+ */
+
+/**
+ * @package charge.phy
+ *
+ * Folder for the physics subsystem.
+ */
+
+/**
+ * @package charge.sys
+ *
+ * Folder for system, resource and IO related parts.
+ */
+
+/**
+ * @package charge.util
+ *
+ * Folder with (often) standalone utilities.
+ */
+
+/**
+ * @package charge.platform
+ *
+ * Folder for code that deal with platform specific things.
+ */
+
+/**
+ * @package charge.platform.core
+ *
+ * Folder for Core implementations.
+ */
+
+/**
+ * @package charge.game
+ *
+ * Folder for higher level classes and functions using other subsystems.
+ */
