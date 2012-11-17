b10336d26d18b0ae35ad2a319af89f99c0ba261c
Jakob Bornecrantz <wallbraker@gmail.com>
2012-11-17 04:04:45 +0100

    box: Add all the needed framework for testing delta compression
    Signed-off-by: Jakob Bornecrantz <wallbraker@gmail.com>

--- src/box/game.d
+++ src/box/game.d
@@ -2,6 +2,8 @@
 // See copyright notice in src/charge/charge.d (GPLv2 only).
 module box.game;
 
+import std.stdio : writefln;
+import std.math : PI;
 import charge.charge;
 import charge.game.actors.primitive;
 
@@ -14,22 +16,40 @@ class Game : GameRouterApp
 private:
 	mixin SysLogging;
 
+	bool server;
+	string address = "localhost";
+	ushort port = 21337;
 
 public:
 	this(string[] args)
 	{
+		parseArgs(args);
+
 		/* This will initalize Core and other important things */
 		auto opts = new CoreOptions();
 		opts.title = "Box";
-		opts.flags = coreFlag.AUTO;
+		opts.flags = server ? coreFlag.PHY : coreFlag.AUTO;
 		super(opts);
 
-		push(new ClientRunner(this));
+
+
+		if (!server)
+			push(new ClientRunner(this, address, port));
+		else
+			push(new ServerRunner(this, port));
 	}
 
 	void close()
 	{
 		super.close();
+	}
+
+	void parseArgs(string[] args)
+	{
+		foreach(a; args) {
+			if (a == "-server")
+				server = true;
+		}
 	}
 }
 
@@ -47,10 +67,12 @@ class World : GameWorld
 /**
  * Client runner.
  */
-class ClientRunner : GameRunner
+class ClientRunner : GameRunner, NetConnectionListener
 {
 public:
 	World w;
+
+	NetClient client;
 
 	GameRouter r;
 	GfxRenderer renderer;
@@ -60,12 +82,19 @@ public:
 	CtlKeyboard keyboard;
 	CtlMouse mouse;
 
+	Cube c1, c2;
+
 
 public:
-	this(GameRouter r)
+	this(GameRouter r, string address, ushort port)
 	{
 		super(GameRunner.Type.Game);
 		this.r = r;
+
+		client = new NetClient(this);
+		writefln("Connecting to %s:%s", address, port);
+		client.connect(address, port);
+
 
 		keyboard = CtlInput().keyboard;
 		mouse = CtlInput().mouse;
@@ -81,25 +110,32 @@ public:
 
 		new GameStaticCube(w, Point3d(0.0, -5.0, 0.0), Quatd(), 200.0, 10.0, 200.0);
 
-		auto c = new Cube(w);
-		c.position = Point3d(0, 10, 0);
+		c1 = new Cube(w);
+		c1.position = Point3d(0, 10, 0);
 
-		c = new Cube(w);
-		c.position = Point3d(.3, 16, .4);
+		c2 = new Cube(w);
+		c2.position = Point3d(.3, 16, .4);
 
-		light.rotation = Quatd(45, -40, 0);
+		light.position = Point3d();
+		light.rotation = Quatd(-PI/6, -PI/4.2, 0);
+		light.shadow = true;
 		cam.position = Point3d(0, 5, 20);
 		cam.rotation = Quatd();
+		cam.far = 512;
 	}
 
 	void close()
 	{
-		breakApartAndNull(w);
+		client.close();
 		breakApartAndNull(cam);
+		breakApartAndNull(c1);
+		breakApartAndNull(c2);
+		breakApartAndNull(w);
 	}
 
 	void logic()
 	{
+		client.tick();
 		w.tick();
 	}
 
@@ -120,6 +156,30 @@ public:
 	}
 
 
+	/*
+	 *
+	 * Network
+	 *
+	 */
+
+
+	void packet(NetConnection c, NetPacket p)
+	{
+		writefln("Packet");
+	}
+
+	void opened(NetConnection c)
+	{
+		writefln("Connected");
+	}
+
+	void closed(NetConnection c)
+	{
+		writefln("Disconnected");
+		r.quit();
+	}
+
+
 protected:
 	void keyDown(CtlKeyboard kb, int sym, dchar unicode, char[] str)
 	{
@@ -131,27 +191,63 @@ protected:
 /**
  * Server runner.
  */
-class ServerRunner : GameRunner
+class ServerRunner : GameRunner, NetConnectionListener
 {
 public:
+	World w;
+
 	GameRouter r;
+	NetServer server;
+	NetConnection con;
+
+	uint counter;
+	PhyStatic cube;
+	PhyCube c1, c2;
 
 
 public:
-	this(GameRouter r)
+	this(GameRouter r, ushort port)
 	{
 		super(GameRunner.Type.Game);
 		this.r = r;
+		server = new NetServer(port, this);
+		writefln("Listening on port %s", port);
+
+		w = new World();
+
+		assert(w !is null);
+		assert(w.phy !is null);
+
+		cube = new PhyStatic(w.phy, new PhyGeomCube(200.0, 10.0, 200.0));
+		cube.position = Point3d(0.0, -5.0, 0.0);
+
+		c1 = new PhyCube(w.phy, Point3d(0, 10, 0));
+
+		c2 = new PhyCube(w.phy, Point3d(.3, 16, .4));
 	}
 
 	void close()
 	{
-
+		server.close();
+		breakApartAndNull(c1);
+		breakApartAndNull(c2);
+		breakApartAndNull(cube);
 	}
 
 	void logic()
 	{
+		w.tick();
+		server.tick();
 
+		if (counter++ >= 240) {
+			c1.position = Point3d(0, 10, 0);
+			c2.position = Point3d(.3, 11.1, .4);
+			counter = 0;
+		}
+
+		if (counter % 10 == 0 && con !is null) {
+
+		}
 	}
 
 	void assumeControl()
@@ -165,4 +261,33 @@ public:
 	}
 
 	void render(GfxRenderTarget rt) { assert(false); }
+
+
+	/*
+	 *
+	 * Network
+	 *
+	 */
+
+
+	void packet(NetConnection c, NetPacket p)
+	{
+		writefln("Packet");
+	}
+
+	void opened(NetConnection c)
+	{
+		writefln("Connected %s", c.address);
+		if (con !is null)
+			con.close();
+		con = c;
+	}
+
+	void closed(NetConnection c)
+	{
+		writefln("Disconnected %s", c.address);
+
+		if (c is con)
+			con = null;
+	}
 }
